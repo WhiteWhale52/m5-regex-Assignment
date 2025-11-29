7dc0114168bab0791b6ce7b5af17735a6b884d7e
Mohamed Tarek <mohamedsama1212@gmail.com>
2025-11-28 20:01:19 -0500

    Finished this assignment

--- src/main/java/regex/Main.java
+++ src/main/java/regex/Main.java
@@ -30,6 +30,7 @@ public class Main {
     //       string has all of these properties, the method should return true. If it is missing one
     //       or more properties, it should return false.
 
+
     /**
      * Returns whether a given string is non-empty, contains one lower case letter,
      * at least one upper case letter, at least one digit, and meets the minimum length.
@@ -38,8 +39,15 @@ public class Main {
      * @return whether the string satisfies the password requirements
      */
     public static boolean checkForPassword(String str, int minLength) {
-        final boolean propertyOne = Pattern.matches("REPLACE WITH CORRECT REGEX", str);
-        // as needed, modify this code.
+        if (str == null) return false;
+        String regex =  String.format(
+                "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{%d,}$",
+                minLength);
+
+        Pattern pattern = Pattern.compile(regex);
+
+        final boolean propertyOne = pattern.matcher(str).matches();
+
         return propertyOne;
     }
 
@@ -55,9 +63,11 @@ public class Main {
      * @return a list containing the email addresses in the string.
      */
     public static List<String> extractEmails(String str) {
-        final Pattern pattern = Pattern.compile("REPLACE WITH CORRECT REGEX");
-        final Matcher matcher = pattern.matcher(str);
         final List<String> result = new ArrayList<>();
+        if (str == null) return result;
+        final Pattern pattern = Pattern.compile("[A-Za-z0-9._%+-]+@(mail\\.)?utoronto\\.ca");
+        final Matcher matcher = pattern.matcher(str);
+
         while (matcher.find()) {
             result.add(matcher.group());
         }
@@ -76,6 +86,7 @@ public class Main {
      * @return whether str contains the same capital letter twice.
      */
     public static boolean checkForDoubles(String str) {
-        return str.matches("replace with correct regex");
+        if  (str == null) return false;
+        return str.matches(".*([A-Z]).*\\1.*");
     }
 }
